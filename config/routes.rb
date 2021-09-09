aaf74f472631bb0d97c2c1851c47bb5256f6ab02
Peteris <psavickis@icloud.com>
2021-09-09 11:44:16 +0100

    6 seting routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root 'pages#home'
   resources :short_urls
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
