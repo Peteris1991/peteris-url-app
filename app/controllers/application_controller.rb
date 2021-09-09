38b81fc15948cb92a93ba92fcc602212214ca21e
Peteris <psavickis@icloud.com>
2021-09-09 11:48:05 +0100

    7 seting account in app conntroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  set_current_tenant_through_filter
+  before_action :set_current_account
+
+  def set_current_account
+    return unless current_user.present?
+    current_account = current_user.account
+    ActsAsTenant.current_tenant = current_account
+  end
+
+    def after_sign_in_path_for(resource)
+        short_urls_url
+    end
 end
